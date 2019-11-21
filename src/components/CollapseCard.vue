73aae42cc75c30d601f9cd301afc944510177da7
Th3Shadowbroker <webmaster@m4taiori.de>
2019-11-21 19:35:10 +0100

    Fixed collapse card

--- src/components/CollapseCard.vue
+++ src/components/CollapseCard.vue
@@ -9,7 +9,7 @@
             </h2>
         </div>
 
-        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne">
             <div class="card-body">
                 <slot/>
             </div>
